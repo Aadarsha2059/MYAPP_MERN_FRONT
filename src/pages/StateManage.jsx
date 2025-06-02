f15f8e1359e66899fd42ae20367e9cb6125631c7
Aadarsha2059 <dhakalaadarshababu20590226@gmail.com>
2025-06-02 12:22:02 +0545

    updated

--- src/pages/StateManage.jsx
+++ src/pages/StateManage.jsx
@@ -1,3 +1,15 @@
+// create a new component logintest that points to "/login-test"
+// make state for name, email, password
+// make a button submit
+// make a state for messsage
+// show message  in components
+// using useEffect setMessage to "plase provide informations"
+// if name is showhow "salin" setMessage to "welcome salin"
+// check the input on button submit, check of empty input
+// if input is empty setMessage to "provide all informations"
+// else setMessage "congratulations"
+
+
 import React, { useEffect, useState } from 'react';
 
 export default function StateManage() {
---
+++ src/pages/StateeManage.jsx
@@ -0,0 +1,11 @@
+// create a new component logintest that points to "/login-test"
+// make state for name, email, password
+// make a button submit
+// make a state for messsage
+// show message  in components
+// using useEffect setMessage to "plase provide informations"
+// if name is showhow "salin" setMessage to "welcome salin"
+// check the input on button submit, check of empty input
+// if input is empty setMessage to "provide all informations"
+// else setMessage "congratulations"
+
